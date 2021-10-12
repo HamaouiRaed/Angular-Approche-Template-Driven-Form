cd59b34eae4786b1dc6611a9ce4f7d68fd4d94cf
RaedH <raed.hamaoui@esprit.tn>
2021-10-13 00:38:12 +0200

    update ***deploy static method in product service to get list de product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -5,15 +5,15 @@ import { Product } from '../models/Products';
   providedIn: 'root'
 })
 export class ProductService {
-  listProducts!: Product[];
-  constructor() { }
-
-getListProduct():Product[]{
-  this.listProducts = [
+  listProducts = [
     { id: 1, title: "T-shirt 1", price: 18, quantity: 0, like: 0 },
     { id: 2, title: "T-shirt 2", price: 21, quantity: 10, like: 0 },
     { id: 3, title: "T-shirt 3", price: 16, quantity: 8, like: 0 },
   ];
+  constructor() { }
+
+getListProduct(){
+  
 return this.listProducts;
 }
 }
